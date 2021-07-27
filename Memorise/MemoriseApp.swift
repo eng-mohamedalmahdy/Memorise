5d2c7ca3cd504af18678c975c6b67c283ede3603
Mahdy <engmohamedalmahdy@gmail.com>
2021-07-27 05:32:06 +0200

    Initial Commit

---
+++ Memorise/MemoriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriseApp.swift
+//  Memorise
+//
+//  Created by NoobMaster69 on 27/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct MemoriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
